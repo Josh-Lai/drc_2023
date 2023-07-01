f2dd370c384ad512239b25c269acad4a7ccf320d
Joshua Lai <joshua.lai@pumpkineng.com.au>
2023-07-01 14:30:11 +1000

    Added pcl and sensor_msgs dependencies

--- sslam/include/localisation.hpp
+++ sslam/include/localisation.hpp
@@ -6,9 +6,46 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+#include "sensor_msgs/msg/point_cloud2.hpp"
+        
+#include "pcl/common/common.h"
+#include "pcl/kdtree/kdtree.h"
+#include "pcl/kdtree/kdtree_flann.h"
+#include "pcl/point_cloud.h"
+#include "pcl/point_types.h"
+#include "pcl/filters/filter.h"
+
+
+#include "pcl_conversions/pcl_conversions.h"
+
+
 class Localisation : public rclcpp::Node {
     public:
         Localisation();
+        
+    private: 
+        /**
+         * @brief Runs the Simple SLAM implementation to obtain the local transformation
+         * between the last transform and the newest
+         *
+         * @param newPointCloud New sensor reading 
+         *
+         * @return relTransform x,y,z,roll, pitch,yaw Parameters descibing the relative transform
+         */
+        std::vector<float> get_local_transform(sensor_msgs::msg::PointCloud2 newPointCloud);
+        
+        /** 
+         *  @brief create a hypothesis grid based on max min and delta
+         *  
+         *  @note Will be a grid of 6 DOF space
+         */
+
+        std::vector<float> generate_hypothesis_space(std::vector<float> min, std::vector<float> delta, std::vector<float> max);
+
+    private:
+        // Point cloud to identify localisation
+        sensor_msgs::msg::PointCloud2 lastPointCloud_; 
+
 
 };
 
--- sslam/src/localisation.cpp
+++ sslam/src/localisation.cpp
@@ -5,6 +5,20 @@ Localisation::Localisation() : rclcpp::Node("localisation"){
     return;
 }
 
+std::vector<float> get_local_transform(sensor_msgs::msg::PointCloud2 newPointCloud) {
+    // Compare the last point cloud with the previous, maximise the evidence between the two
+
+    //store the previous PointCloud in a K-D Tree
+    //Get the hypothesis grid based on the 
+    
+    //For each Hypothesis:
+    //For each point in newPointCloud * 
+    //  Ej = evidence_to_closest_point
+    //  E += 
+    //
+    return std::vector<float>(6);
+
+}
 
 
 int main(int argc, char* argv[]) {
