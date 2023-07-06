aff7aaf8a9b20724fedfb9577a5547633fc0864a
Shanker D <dshanker134@gmail.com>
2023-07-06 11:22:41 +1000

    rename

--- vision/scripts/opencv_laneDetection.py
+++ vision/scripts/opencv_laneDetection.py
@@ -7,30 +7,35 @@ class Detection(Node):
 
     def __init__(self):
         super().__init__('cv_detection')
+        
+        self.declare_parameter('image_topic', '/camera/color/image_raw')
+        image_topic = self.get_parameter('image_topic').get_parameter_value().string_value
+        
         self.subscription = self.create_subscription(
             Image,
-            'test',
+            image_topic,
             self.detection_callback,
             10)
+
         self.subscription  # prevent unused variable warning
         bridge = CvBridge()
 
     def detection_callback(self, msg):
+        print("hello")
         cv_image = bridge.imgmsg_to_cv2(msg)
-        cv2.imshow("testing", cv_image)
-        cv2.waitKey(1)
+        # cv2.imshow("testing", cv_image)
+        # cv2.waitKey(1)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = Detection()
+    image_subscriber = Detection()
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(image_subscriber)
 
     # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
+
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
 
