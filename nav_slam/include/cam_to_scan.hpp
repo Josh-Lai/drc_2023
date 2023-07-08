85053d5f0836d69f018e21f24b53c9d3e410bd92
Joshua Lai <joshua.lai@pumpkineng.com.au>
2023-07-08 12:11:08 +1000

    Added depth_to_image conversion

--- nav_slam/include/cam_to_scan.hpp
+++ nav_slam/include/cam_to_scan.hpp
@@ -5,9 +5,17 @@
 
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
+/**
+ * @brief Processes Camera data to pass to SLAM toolboxk
+ */
 class CamToScan : public rclcpp::Node {
     public: 
         CamToScan();
+
+    private:
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr camDepthSub_;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cloudPub_;
+        void point_cloud_cb(sensor_msgs::msg::PointCloud2 msg);
 };
 
 
--- nav_slam/launch/launch_slam_toolbox.launch.py
+++ nav_slam/launch/launch_slam_toolbox.launch.py
@@ -4,7 +4,13 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    slam_params = os.path.join(get_package_share_directory("nav_slam"), "config", "slam_params.yaml")
+    slam_params = os.path.join(
+        get_package_share_directory("nav_slam"), "config", "slam_params.yaml")
+    depth_to_scan_config = os.path.join(
+        get_package_share_directory('nav_slam'), 'config', 'depth_conversion_params.yaml')
+    laser_scan_config = os.path.join(
+        get_package_share_directory('nav_slam'), 'config', 'laser_match_param.yaml')
+
     start_slam_node = Node(
             package="slam_toolbox",
             executable="async_slam_toolbox_node", 
@@ -12,8 +18,46 @@ def generate_launch_description():
             output="screen",
             parameters=[slam_params]
     )
+    start_cam_depth_cvt = Node(
+            package="nav_slam",
+            executable="cam_to_scan",
+            name="cam_to_scan"
+    )
+    
+    depth_to_scan = Node(
+            package='depthimage_to_laserscan',
+            executable='depthimage_to_laserscan_node',
+            name='depthimage_to_laserscan_node',
+            remappings=[('depth', '/camera/depth/image_rect_raw'),
+                        ('depth_camera_info', '/camera/depth/camera_info')],
+            parameters=[depth_to_scan_config])
+
+    start_scan_matcher = Node(
+            package="ros2_laser_scan_matcher",
+            executable="laser_scan_matcher",
+            name="laser_scan_matcher",
+            parameters=[laser_scan_config],
+            output="screen")
+
+
+    cam_link = Node(
+             package='tf2_ros',
+             executable='static_transform_publisher',
+             arguments = ['--x', '0', '--y', '0', '--z', '1', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'base_link', '--child-frame-id', 'camera_link']
+    )
+    base_footprint = Node(
+             package='tf2_ros',
+             executable='static_transform_publisher',
+             arguments = ['--x', '0', '--y', '0', '--z', '1', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'camera_link', '--child-frame-id', 'base_footprint']
+    )
+
     ld = LaunchDescription()
 
-    ld.add_action(start_slam_node)
+    #ld.add_action(start_cam_depth_cvt)
+    ld.add_action(depth_to_scan)
+    #ld.add_action(start_slam_node)
+    ld.add_action(start_scan_matcher)
+    ld.add_action(cam_link);
+    ld.add_action(base_footprint);
     return ld
 
