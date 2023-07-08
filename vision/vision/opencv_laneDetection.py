25dfda6e30c7c80564dcec3e125340cb29f82238
xAKLM <ash.masame@outlook.com>
2023-07-08 15:20:26 +1000

    fixed line for np.cfloat

--- vision/vision/opencv_laneDetection.py
+++ vision/vision/opencv_laneDetection.py
@@ -8,6 +8,7 @@ import cv2
 from geometry_msgs.msg import Point
 import numpy as np
 from cv_msgs.msg import LanePixels
+import sensor_msgs.msg._point_cloud2 as pc2
 
 class Detection(Node):
 
@@ -31,54 +32,19 @@ class Detection(Node):
 
     def detection_callback(self, msg):
         self.i+=1
-        array = pointcloud2_to_xyz_array(msg)
-        print(array)
-        self.get_logger().info('%d Images Received' % self.i)
-        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR )
-        self.publisher.publish(self.get_lane_data(cv_image, self.yellow_limits, self.blue_limits))
-        cv2.imshow("image_raw", cv_image)
-        cv2.waitKey(1)
-
-    # Returns the detected yellow and blue lanes within the image
-    # yellow_limits/blue_limits is just a tuple of lower_hsv_range,upper_hsv_range
-    def get_lane_data(self, image, yellow_limits, blue_limits):
-        yellow_lanes =  self._find_single_lane(image, yellow_limits[0], yellow_limits[1])
-        blue_lanes = self._find_single_lane(image, blue_limits[0], blue_limits[1])
+        self.get_logger().info('%d Received Point Cloud' % self.i)
+        self.filter_point_cloud(msg)
         
-        # Float XX multi_array
-        lane_data = LanePixels()
-        lane_data.yellow_lane_pixels = self.convert_contour_to_points(yellow_lanes)
-        lane_data.blue_lane_pixels = self.convert_contour_to_points(blue_lanes)
 
-        return lane_data
+    # Filters the Pointcloud2 message to only see yellow and blue 
+    def filter_point_cloud(self, pointcloud2msg):
+        filtered_point_cloud = []
+        pointcloud_array = pointcloud2_to_xyz_array(pointcloud2msg)
 
-    def convert_contour_to_points(self, contour):
-        point_msgs = []
 
-        for point in contour:
-            point_msg = Point()
-            point_msg.x = point[0][0]
-            point_msg.y = point[0][1]
-            point_msg.z = 0.0  # Set the z-coordinate if needed
-            point_msgs.append(point_msg)
 
-        return point_msgs
-
-    def _find_single_lane(self, image, lower_thresh, upper_thresh):
-        # Convert the image to the HSV color space
-        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        # Threshold the image to obtain a mask of the lane
-        mask = cv2.inRange(hsv_image, lower_thresh, upper_thresh)
-
-        # Find contours in the mask
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        if len(contours) > 0:
-            # Find the contour with the largest area
-            return max(contours, key=cv2.contourArea)
+        return filtered_point_cloud
         
-        return None
     
 
 def main(args=None):
@@ -237,7 +203,7 @@ def split_rgb_field(cloud_arr):
     return new_cloud_arr
 
 
-def get_xyz_points(cloud_array, remove_nans=True, dtype=np.float):
+def get_xyz_points(cloud_array, remove_nans=True, dtype=np.cfloat):
     if remove_nans:
         mask = np.isfinite(cloud_array['x']) & np.isfinite(cloud_array['y']) & np.isfinite(cloud_array['z'])
         cloud_array = cloud_array[mask]
