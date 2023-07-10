dbe984b28864804c25687cc0ffcd295894fdce58
Shanker-RPi <dshanker134@gmail.com>
2023-07-10 17:00:58 +1000

    Cleaned up code to build controller code

--- navigation/src/drc_controller.cpp
+++ navigation/src/drc_controller.cpp
@@ -1,15 +1,22 @@
 #include "navigation/drc_controller.hpp" 
 
-Controller::Controller : Node("controller") { 
-  cmd_vel_sub_ = create_subscription<Twist>("cmd_vel", 10, std::bind(&Controller::cmd_vel_cb, this _1)); 
+Controller::Controller() : Node("controller") { 
+  cmd_vel_sub_ = create_subscription<Twist>("cmd_vel", 10, std::bind(&Controller::cmd_vel_cb, this, _1)); 
   speed_pub_ = create_publisher<Int32>("speed", 10);
   steer_pub_ = create_publisher<Int32>("steer", 10);  
 } 
 
-Controller::cmd_vel_cb(Twist::SharedPtr msg) { 
+void Controller::cmd_vel_cb(Twist::SharedPtr msg) { 
   Int32 speed_msg, steer_msg; 
   speed_msg.data = msg->linear.x; 
-  steer_msg.data = msd->angular.z; 
+  steer_msg.data = msg->angular.z; 
   speed_pub_->publish(speed_msg); 
   steer_pub_->publish(steer_msg);
+}
+
+int main(int argc, char** argv) { 
+  rclcpp::init(argc, argv); 
+  rclcpp::spin(std::make_shared<Controller>()); 
+  rclcpp::shutdown();
+  return 0; 
 }
