c451f390ee149e047c2081937c220cb931c144e4
Joshua Lai <joshua.lai@pumpkineng.com.au>
2023-07-10 19:29:36 +1000

    Changed cam_to_scan node to publish lane points
    Lane_points published on 2D point cloud in line with the scan

--- nav_slam/include/cam_to_scan.hpp
+++ nav_slam/include/cam_to_scan.hpp
@@ -4,9 +4,14 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/point_cloud2_iterator.hpp"
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 /**
- * @brief Processes Camera data to pass to SLAM toolboxk
+ * @brief Converts the point cloud to a flattened point cloud for a scan
  */
 class CamToScan : public rclcpp::Node {
     public: 
@@ -16,6 +21,9 @@ class CamToScan : public rclcpp::Node {
         rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr camDepthSub_;
         rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cloudPub_;
         void point_cloud_cb(sensor_msgs::msg::PointCloud2 msg);
+        
+
+        const float pclHeight = 0;
 };
 
 
--- nav_slam/src/cam_to_scan.cpp
+++ nav_slam/src/cam_to_scan.cpp
@@ -5,8 +5,8 @@ CamToScan::CamToScan() : rclcpp::Node("cam_to_scan") {
     RCLCPP_INFO(this->get_logger(), "Initialised cam depth node");
 
     camDepthSub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            "/camera/depth/color/points",
-            10, 
+            "/lane_points",
+            rclcpp::QoS(5).best_effort(),
             std::bind(&CamToScan::point_cloud_cb, this, _1)
     );
 
@@ -15,7 +15,26 @@ CamToScan::CamToScan() : rclcpp::Node("cam_to_scan") {
 }
 
 void CamToScan::point_cloud_cb(sensor_msgs::msg::PointCloud2 msg) {
-    cloudPub_->publish(msg);
+    //Convert the point cloud into a 2D point cloud
+    //
+    //Iterate over all the points, set them at y = 0;
+    pcl::PointCloud<pcl::PointXYZ> pclPointCloud;
+    sensor_msgs::PointCloud2Iterator<float> iter_x(msg, "x");
+    pcl::PointXYZ pclPoint;
+    for(; iter_x != iter_x.end(); ++iter_x) {
+        pclPoint.x = iter_x[0];
+        pclPoint.y = pclHeight;
+        pclPoint.z = iter_x[2];
+        pclPointCloud.push_back(pclPoint);
+    }
+    //Convert this into PointCloud2
+    sensor_msgs::msg::PointCloud2 out;
+    pcl::toROSMsg(pclPointCloud, out);
+    out.header.stamp = msg.header.stamp;
+    out.header.frame_id = msg.header.frame_id;
+
+    
+    cloudPub_->publish(out);
 }
 
 int main(int argc, char* argv[]) {
